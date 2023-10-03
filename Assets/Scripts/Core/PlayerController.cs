6a8af11eadc98d9a7dfcbb3c251792f3c3b97ebc
Tayler Parsons <upsilon@diesbackwards.com>
2023-10-03 02:47:54 +0100

    add functionality to record two morse code prosigns, changes UI elements depending on which prosign is inputted (slightly borked)

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    // Settings to control the timings
     [SerializeField] private float holdTime = 0.0f; // How long the player has held down the input key for.
-    [SerializeField] private float DotToDashThreshold = 0.3f; // The time that differentiates between a dash and a dot.
+    [SerializeField] private float DotToDashThreshold = 0.3f; // The time that differentiates between a dash and a dot. (1 = 1sec)
+    
+    // Inputted Morse Code Array
+    [SerializeField] private ProsignType[] morseInput = new ProsignType[2];
+    [SerializeField] private int currentInputIndex = 0;
+    
+    // Coroutine
+    private Coroutine noInputClear;
+    [SerializeField] private float AutomaticClearDelay = 2.0f;
+
+    // UI
+    [SerializeField] Image directionIndex1;
+    [SerializeField] Image directionIndex2;
+    [SerializeField] Image timeIndex1;
+    [SerializeField] Image timeIndex2;
+
+    [SerializeField] Sprite dotProsign;
+    [SerializeField] Sprite dashProsign;
+
 
     private enum ProsignType {
         Dot,
@@ -16,17 +36,39 @@ public class PlayerController : MonoBehaviour
     void Update() {
         if (Input.GetKey(KeyCode.Space)) {
             float hTime = GetHoldDownTime();
-            Debug.Log(GetProsign(hTime));
+
+            // Debug.Log(GetProsign(hTime));
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            ProsignType pSign = GetProsign(holdTime);
+            morseInput[currentInputIndex] = pSign;
+            currentInputIndex = (currentInputIndex + 1) % 2;
+
             holdTime = 0.0f;
+        
+            if (noInputClear != null) {
+                StopCoroutine(noInputClear);
+            }
+            noInputClear = StartCoroutine(AutomaticClearAfterDelay());
         }
+
+        UpdateUI();
+
+        // // DEBUG!! REMOVE ON BUILD! 
+        // if (Input.GetKeyDown(KeyCode.C))
+        // {
+        //     ClearMorseInput();
+        // }
     }
 
     float GetHoldDownTime() {
         holdTime += Time.deltaTime;
         return holdTime;
+    }
+
+    void ClearMorseInput() {
+        morseInput[0] = ProsignType.Pause;
+        morseInput[1] = ProsignType.Pause;
     }
 
     ProsignType GetProsign(float holdTime) {
@@ -36,5 +78,31 @@ public class PlayerController : MonoBehaviour
             return ProsignType.Dash;
         }
         return ProsignType.Pause;
+    }
+
+    IEnumerator AutomaticClearAfterDelay() { // If the user hasn't inputted in X seconds, clear the first entry so they can try again
+        yield return new WaitForSeconds(AutomaticClearDelay);
+
+        ClearMorseInput();
+        noInputClear = null;
+
+        Debug.Log("No input! Automatically reset input array.");
+    }
+
+    void UpdateUI() {
+        /* Lord, please have mercy on my soul for what I am about to do. I do not take pride in it,
+           It is 2:37 AM, I need sleep. */
+        
+        if (morseInput[0] == ProsignType.Dot) {
+            directionIndex1.sprite = dotProsign;
+        } else if (morseInput[0] == ProsignType.Dash) {
+            directionIndex1.sprite = dashProsign;
+        }
+
+        if (morseInput[1] == ProsignType.Dot) {
+            directionIndex1.sprite = dotProsign;
+        } else if (morseInput[1] == ProsignType.Dash) {
+            directionIndex1.sprite = dashProsign;
+        }
     }
 }
