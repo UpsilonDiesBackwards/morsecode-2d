f3c0b9de0d542b325be3d779a11ad146d3130539
Tayler Parsons <reeceparsons666@gmail.com>
2023-10-12 13:29:48 +0100

    add very wonkey movement system

--- Assets/Scripts/Core/Controllers/PlayerAgentController.cs
+++ Assets/Scripts/Core/Controllers/PlayerAgentController.cs
@@ -5,45 +5,46 @@ using UnityEngine;
 
 public class PlayerAgentController : MonoBehaviour
 {
-    public int[] directionHalf = new int[2];
-    public int[] timeHalf = new int[2];
+    public float gridSize = 0.9f;
+
+    private Vector2 directionToMove;
+    private int timeToMove;
+
+    [SerializeField] private int[] directionHalf = new int[2];
+    [SerializeField] private int[] timeHalf = new int[2];
 
     public void InterpretMorse(int[] controllerArray) {
         Array.Copy(controllerArray, 0, directionHalf, 0 , 2); // Split the controller array into two arrays
         Array.Copy(controllerArray, 2, timeHalf, 0 , 2);
 
+        Debug.Log("dir half: " + directionHalf[0] + directionHalf[1]);
+                Debug.Log("time half: " + timeHalf[0] + timeHalf[1]);
+
+
         // this is hell. I apologise
 
-        if (directionHalf[0] == 0 && directionHalf[1] == 0) {
-            Debug.Log("Move Right");
-        }
+        if (directionHalf[0] == 0 && directionHalf[1] == 0) { directionToMove = new Vector2(1, 0); }
 
-        if (directionHalf[0] == 0 && directionHalf[1] == 1) {
-            Debug.Log("Move Upwards");
-        }
+        if (directionHalf[0] == 0 && directionHalf[1] == 1) { directionToMove = new Vector2(0, 1); }
 
-        if (directionHalf[0] == 1 && directionHalf[1] == 0) {
-            Debug.Log("Move Downwards");
-        }
+        if (directionHalf[0] == 1 && directionHalf[1] == 0) { directionToMove = new Vector2(0, -1); }
 
-        if (directionHalf[0] == 1 && directionHalf[1] == 1) {
-            Debug.Log("Move Left");
-        }
+        if (directionHalf[0] == 1 && directionHalf[1] == 1) { directionToMove = new Vector2(-1, 0); }
 
-        if (timeHalf[0] == 0 && timeHalf[1] == 0) {
-            Debug.Log("Move for 1 tile");
-        }
+        if (timeHalf[0] == 0 && timeHalf[1] == 0) { timeToMove = 0; }
 
-        if (timeHalf[0] == 0 && timeHalf[1] == 1) {
-            Debug.Log("Move for 2 tiles");
-        }
+        if (timeHalf[0] == 0 && timeHalf[1] == 1) { timeToMove = 1; }
 
-        if (timeHalf[0] == 1 && timeHalf[1] == 0) {
-            Debug.Log("Move for 3 tiles");
-        }
+        if (timeHalf[0] == 1 && timeHalf[1] == 0) { timeToMove = 2; }
 
-        if (timeHalf[0] == 1 && timeHalf[1] == 1) {
-            Debug.Log("Move for 4 tiles");
+        if (timeHalf[0] == 1 && timeHalf[1] == 1) { timeToMove = 3; }
+
+        Move(directionToMove, timeToMove);
+    }
+
+    void Move(Vector2 direction, int time) {
+        for (int i = 0; i <= time; i++) {
+            transform.position = new Vector3(direction.x * gridSize, direction.y * gridSize, 0f);
         }
     }
 }
